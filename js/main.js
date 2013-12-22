35b09758aa53a20a48691b551ec6151bab6feb2c
Atsuki Demizu <demmys.atsuki@gmail.com>
2013-12-22 21:45:50 +0900

    implemented: 動画から鼎談まで

--- js/main.js
+++ js/main.js
@@ -9,4 +9,20 @@
 $(function(){
     impress().init();
     var api = impress();
+    var $video = $("#intro>video");
+    var endTime = 12.5;
+    var played = false;
+    $video.bind("timeupdate", function(){
+        if(endTime > 0 && $video[0].currentTime > endTime){
+            endTime = 0;
+            api.next();
+        }
+    });
+    $(window).keydown(function(e){
+        if(!played && e.keyCode == 13){
+            played = true;
+            $video.css("opacity", "1");
+            $video[0].play();
+        }
+    });
 });
